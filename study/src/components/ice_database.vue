3c73cf1871efb3d8819b541bf6c41f2bd423ebd3
shibuyakosei <kosei.shibuya@e-grant.net>
2022-01-27 08:54:28 +0900

    エラーが出ている状態（ ;  ; ）

---
+++ study/src/components/ice_database.vue
@@ -0,0 +1,10 @@
+<script>
+const ice = [
+  {
+    img_path: "../../public/img/ガリガリ君.jpeg",
+    name: "ガリガリ君 ソーダ",
+    price: "60円",
+  },
+];
+export { ice };
+</script>
--- study/src/components/ice_info.vue
+++ study/src/components/ice_info.vue
@@ -3,13 +3,19 @@
     <img class="ice" :src="img" />
     <p>{{ name }}</p>
     <p>{{ price }}</p>
+    <ice></ice>
   </div>
 </template>
 
 <script>
+import { ice } from "./ice_database.vue";
 const name = "ガリガリ君";
 const price = "60円";
+
 export default {
+  components: {
+    ice,
+  },
   data: function () {
     return {
       img: require("../../public/img/ガリガリ君.jpeg"),
